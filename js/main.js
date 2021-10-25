72d6c2f7716eae66e04fbc9e5ea8ca8c3a3ac377
Daria <wwdarcool@yandex.ru>
2021-10-26 01:25:24 +0300

    add: adaptive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider__button-next',
+    prevEl: '.slider__button-prev',
+  },
+
+});
